2550e9caa10b34826a1878390c6e80a2f7052dd0
rakhatyusupov <seminar.p2p@gmail.com>
2025-03-04 11:22:44 +0300

    re-add: design system lol

--- src/index.js
+++ src/index.js
@@ -4,15 +4,7 @@ import data from './data/data.json'
 import './stylesheets/00_DataTables.css'
 import '../src/index.css'
 
-const imagesContext = require.context(
-  './images/datatable/',
-  false,
-  /\.(png|jpe?g|gif)$/
-)
-
 $(document).ready(function () {
-  let currentImagePath = null
-
   $('#main').DataTable({
     data: data,
     columns: [
@@ -28,51 +20,12 @@ $(document).ready(function () {
     language: {
       info: '_START_ - _END_ of _TOTAL_ entries',
       lengthMenu: 'Show _MENU_ entries',
-      search: 'Search:',
+      search: '', // Removed label from dt-search
       paginate: {
         previous: '‹',
         next: '›'
       }
     },
-    dom: '<"top"f>rt<"bottom"lip><"clear">',
-    createdRow: function (row, data) {
-      $(row).hover(
-        function () {
-          const imagePath = imagesContext(`./${data.image}`)
-
-          if (currentImagePath !== imagePath) {
-            currentImagePath = imagePath
-
-            $('.image-preview').remove()
-
-            const $preview = $(`<img src="${imagePath}" class="image-preview">`)
-            $('body').append($preview)
-
-            positionImage($preview)
-          }
-        },
-        function () {}
-      )
-    }
+    dom: '<"top"f>rt<"bottom"lip><"clear">'
   })
-
-  $(window).resize(function () {
-    positionImage($('.image-preview'))
-  })
-
-  function positionImage($img) {
-    if ($img.length) {
-      const windowHeight = $(window).height()
-      const windowWidth = $(window).width()
-      const imgHeight = $img.height()
-      const imgWidth = $img.width()
-
-      $img.css({
-        position: 'fixed',
-        top: (windowHeight - imgHeight) / 2,
-        left: (windowWidth - imgWidth) / 2,
-        zIndex: 1000
-      })
-    }
-  }
 })
