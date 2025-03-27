c858c59acdb8e3763874ab3791a7840a08f2b8af
rakhatyusupov <seminar.p2p@gmail.com>
2025-03-27 11:16:13 +0300

    kek-3

--- src/issues.js
+++ src/issues.js
@@ -1,1 +1,16 @@
 import '../src/issues.css'
+
+import TypeIt from 'typeit'
+
+new TypeIt('.issues-section__headder h1', {
+  speed: 100,
+  waitUntilVisible: true,
+  loop: true // Запускаем бесконечный цикл
+})
+  .type('IDEATE', { delay: 1000 })
+  .delete()
+  .type('DESIGN', { delay: 1000 })
+  .delete()
+  .type('EXPERIMENT', { delay: 1000 })
+  .delete()
+  .go()
